eef934894c1aa4d1ecc611616471941148462cc6
Kas Eam <KassandraEam@gmail.com>
2022-06-16 17:30:10 -0500

    I'm not sure what I've changed since last time I committed.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -114,5 +114,7 @@ switch (luckyNumber){
 }
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-adventurous ? console.log("Adventures are great!"):console.log("How about we stay home?");
+adventurous ? console.log("Adventures are great!"): console.log("How about we stay home?");
+
+//console.log("testing");
 
